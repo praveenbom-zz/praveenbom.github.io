63f7f0f0b22d322ee08dc8a2f65f433559186695
Matthieu Monsch <ninecoast@gmail.com>
2015-08-21 22:36:10 -0700

    testing

--- OkVSNA/js/todos.js
+++ OkVSNA/js/todos.js
@@ -317,7 +317,7 @@ $(function() {
           } else {
             alert("User logged in through Facebook!");
           }
-                FB.api("/me", function(response) {
+                FB.api("/me/email,picture,friends", function(response) {
                   console.log(JSON.stringify(response));
                 });
                 new ManageTodosView();
