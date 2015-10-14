fb4f889daf4c75f8729eb7ef8364f037d5e7c31f
Matthieu Monsch <ninecoast@gmail.com>
2015-10-13 22:20:15 -0700

    testing

--- OkVSNA/js/todos.js
+++ OkVSNA/js/todos.js
@@ -581,12 +581,12 @@ $(function() {
     },
 
     closeName: function() {
-      console.log(Parse.User.current());
+      console.log(Parse.User.current().escape("contact_email"));
       Parse.User.current().set("first_name", this.input_display_name.val());
       Parse.User.current().save(null, {
         success: function(user) {
           console.log("great success");
-          console.log(Parse.User.current());
+          console.log(Parse.User.current().escape("contact_email"));
         },
         error: function(user) {
           console.log("fail");
