ced8e1ce99d4971cc25e9b8b1cb68f3d55b9f524
Matthieu Monsch <ninecoast@gmail.com>
2015-10-13 21:41:43 -0700

    testing

--- OkVSNA/js/todos.js
+++ OkVSNA/js/todos.js
@@ -582,7 +582,7 @@ $(function() {
 
     closeName: function() {
       console.log("closing");
-      Parse.User.current().set("first_name", this.input.val())
+      Parse.User.current().set("first_name", this.input_display_name.val());
       Parse.User.current().save(null, {
         success: function(user) {
           console.log("great success");
