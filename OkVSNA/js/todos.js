b9e64b716c77dd67e5df4371d2a361c8790fad48
Matthieu Monsch <ninecoast@gmail.com>
2015-10-16 20:58:22 -0700

    testing

--- OkVSNA/js/todos.js
+++ OkVSNA/js/todos.js
@@ -584,6 +584,7 @@ $(function() {
     closeField: function(e) {
       var el = $(e.target);
       var fieldName = el.attr("id");
+      console.log("setting " + this.$("#"+fieldName+"-input").val());
       Parse.User.current().set(fieldName, this.$("#"+fieldName+"-input").val());
       Parse.User.current().save(null, {
         success: function(user) {
