84288ae7b5a847196deefd460eee98e2a0534917
Matthieu Monsch <ninecoast@gmail.com>
2015-10-16 19:30:19 -0700

    testing

--- OkVSNA/js/todos.js
+++ OkVSNA/js/todos.js
@@ -385,6 +385,8 @@ $(function() {
     // Our template for the line of statistics at the bottom of the app.
     statsTemplate2: _.template($('#stats-template2').html()),
 
+    statsTemplate3: _.template($('#profile-item').html()),
+
     // Our template for the line of statistics at the bottom of the app.
 //    profileItemTemplate: _.template($('#profile-item').html()),
 
