3a337e939d903d2a1b64b2b8084115228e8593e8
Matthieu Monsch <ninecoast@gmail.com>
2015-10-18 12:16:28 -0700

    testing

--- OkVSNA/js/todos.js
+++ OkVSNA/js/todos.js
@@ -432,6 +432,10 @@ $(function() {
         label: "occupation",
         field_name: "Occupation"
       }));
+      this.$('#public-profile').append(this.profileItemTemplate({
+        label: "diet",
+        field_name: "Diet"
+      }));
       this.$('#interested-in').append(this.profileItemTemplate({
         label: "min_age",
         field_name: "Min age"
