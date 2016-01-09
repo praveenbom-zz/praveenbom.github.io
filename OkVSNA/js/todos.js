e77a6016194d0f5f320d6d4236b74e57025dd882
Matthieu Monsch <ninecoast@gmail.com>
2016-01-09 12:21:40 -0800

    testing

--- OkVSNA/js/todos.js
+++ OkVSNA/js/todos.js
@@ -147,7 +147,7 @@ $(function() {
     render: function() {
       console.log(this.template.toString());
       console.log("b");
-      console.log(this.model.toString());
+      console.log(this.model.escape("username"));
       $(this.el).html(this.template(this.model.toJSON()));
       this.input = this.$('.edit');
       return this;
