25f3698243126e4693b86d4c25c0e65bea177a69
Matthieu Monsch <ninecoast@gmail.com>
2016-04-30 17:40:39 -0700

    testing

--- OkVSNA2/js/okvsna.js
+++ OkVSNA2/js/okvsna.js
@@ -338,9 +338,9 @@ $(function() {
 
       Parse.User.current().set(label, fieldVal);
       if (label.indexOf("birth") > -1) {
-        month_map = {"January": 1, "February": 2, "March": 3, "April": 4,·
+        month_map = {"January": 1, "February": 2, "March": 3, "April": 4,
                      "May": 5, "June": 6, "July": 7, "August": 8,
-                     "September": 9, "October": 10, "November": 11,·
+                     "September": 9, "October": 10, "November": 11,
                      "December": 12}
         var y = Number(Parse.User.current().escape("birth_year"));
         var m = month_map[Parse.User.current().escape("birth_month")];
