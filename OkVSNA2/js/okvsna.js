d55ad1a0bd52176f6a2f22625da717dfdf5a0636
Matthieu Monsch <ninecoast@gmail.com>
2016-06-06 22:42:22 -0700

    testing

--- OkVSNA2/js/okvsna.js
+++ OkVSNA2/js/okvsna.js
@@ -548,6 +548,7 @@ $(function() {
       var self = this;
       if (e.keyCode != 13) return;
 
+      console.log("why");
       this.convoMessages.create({
         fromUser: Parse.User.current().escape("username"),
         toUser: this.toUser,
